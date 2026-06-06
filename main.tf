199cc514d26097fff8712605cc17ec68a904f447
danyflow <danyfloresgarcia18@gmail.com>
2026-06-06 18:46:06 -0500

    Agregar y configurar ec2, key pair y group security

--- main.tf
+++ main.tf
@@ -5,11 +5,51 @@ provider "aws" {
 
 
 ### resource ######
-resource "aws_instance" "nginx-example" {
+resource "aws_instance" "nginx-server" {
   ami           = "ami-029a761f237195c2c"
   instance_type = "t3.micro"
 
   tags = {
     Name = "NginxExampleInstance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y nginx
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
+
+  key_name               = aws_key_pair.nginx-server-ssh.key_name
+  vpc_security_group_ids = [aws_security_group.nginx-server-sg.id]
+}
+
+resource "aws_key_pair" "nginx-server-ssh" {
+  key_name   = "nginx-server-ssh"
+  public_key = file("nginx-server.key.pub")
+}
+
+resource "aws_security_group" "nginx-server-sg" {
+  name = "nginx-server-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
